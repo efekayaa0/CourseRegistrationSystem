b76a93bab26892a633730df804a69e953c1af492
Efe Kaya <efe.kaya26@hotmail.com>
2026-06-18 19:15:48 +0300

    first commit

---
+++ CourseRegistrationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CourseRegistrationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
